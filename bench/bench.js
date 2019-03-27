b545c144a14adb14b615a7bd48d668a143ae163c
Yawning Angel <yawning@schwanenlied.me>
2019-03-27 10:33:05 +0000

    Add trivial benchmarks
    High performance, this is not.  Note that each ECB operation being
    benchmarked is doing 2 blocks at a time.

---
+++ bench/bench.js
@@ -0,0 +1,46 @@
+// Copyright (c) 2019 Oasis Labs Inc. <info@oasislabs.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+// BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+var aes = require('../aes');
+var Benchmark = require('benchmark');
+
+
+var ecb128 = new aes.ECB(Buffer.alloc(16));
+var ecb192 = new aes.ECB(Buffer.alloc(24));
+var ecb256 = new aes.ECB(Buffer.alloc(32));
+var src = Buffer.alloc(16);
+var dst = Buffer.alloc(16);
+
+var suite = new Benchmark.Suite;
+suite.add('ECB-AES128', function() {
+	ecb128.encrypt(dst, src);
+})
+	.add('ECB-AES192', function() {
+		ecb192.encrypt(dst, src);
+	})
+	.add('ECB-AES256', function() {
+		ecb256.encrypt(dst, src);
+	})
+	.on('cycle', function(event) {
+		console.log(String(event.target)); // eslint-disable-line no-console
+	})
+	.run();
